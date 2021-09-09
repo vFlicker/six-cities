b77ab44dab7090a20de59f449b1361943d613d87
Vladislav Flicker <flickervladislav@gmail.com>
2021-09-10 02:01:29 +0300

    Configure ESlint for TypeScript types checking.

---
+++ src/app/app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App(): React.ReactElement {
+  return (
+    <h1>React TypeScript App</h1>
+  );
+}
+
+export default App;
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { Configuration as WebpackConfiguration } from 'webpack';
 import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 interface Configuration extends WebpackConfiguration {
   devServer: WebpackDevServerConfiguration;
@@ -11,19 +12,27 @@ const config: Configuration = {
   entry: './src/index.tsx',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './public')
+    path: path.resolve(__dirname, './public'),
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
     ],
   },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      eslint: {
+        files: './src/**/*',
+      },
+    }),
+  ],
   resolve: {
-    extensions: ['.js', '.ts', '.tsx']
+    extensions: ['.js', '.ts', '.tsx'],
   },
   devServer: {
     static: path.resolve(__dirname, 'public'),
