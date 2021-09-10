7b91c3bdd7a5f96382ec80bce194cde2a185f74c
Vladislav Flicker <flickervladislav@gmail.com>
2021-09-10 16:57:07 +0300

    Add React-components.

--- src/app/app.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function App(): React.ReactElement {
-  return (
-    <h1>React TypeScript App</h1>
-  );
-}
-
-export default App;
---
+++ src/components/app/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from '../main/main';
+import Header from '../header/header';
+
+function App(): React.ReactElement {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
+
+export default App;
