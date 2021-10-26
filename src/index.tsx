36322d8b688e91be0362a29af584079112148df5
Vladislav Flicker <flickervladislav@gmail.com>
2021-10-26 17:47:53 +0300

    Connect Redux to the project.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import { reducer } from './store/reducer';
 import { offers, reviews } from './mocks';
 import App from './components/app';
 
+const store = createStore(
+  reducer,
+  composeWithDevTools(),
+);
+
 ReactDOM.render(
-  <App
-    offers={offers}
-    reviews={reviews}
-  />,
+  <StrictMode>
+    <Provider store={store}>
+      <App
+        offers={offers}
+        reviews={reviews}
+      />
+    </Provider>
+  </StrictMode>,
   document.getElementById('root'),
 );
