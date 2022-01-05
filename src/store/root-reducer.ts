3c3adb5dae7713ab6fe04c21575007d8c0303824
Vladislav Flicker <flickervladislav@gmail.com>
2022-01-05 22:04:37 +0200

    refactor: use createSlice for appProcess instead of createReducer.

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import { ReducerName } from '../const';
-import appProcess from './app-process';
+// import appProcess from './app-process';
+import appProcessReducer from './app-process/slice';
 import offerData from './offer-data';
 import offersData from './offers-data';
 import offersFavoriteData from './offers-favorite-data';
@@ -8,7 +9,8 @@ import offerNearbyData from './offers-nearby-data';
 import userProcess from './user-process';
 
 const rootReducer = combineReducers({
-  [ReducerName.AppProcess]: appProcess,
+  // [ReducerName.AppProcess]: appProcess,
+  [ReducerName.AppProcess]: appProcessReducer,
   [ReducerName.OfferData]: offerData,
   [ReducerName.OffersData]: offersData,
   [ReducerName.OffersFavoriteData]: offersFavoriteData,
