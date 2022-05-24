4453844974ab0b8df34dbddc0ef150be500440c7
vFlicker <flickervladislav@gmail.com>
2022-05-24 03:29:20 +0300

    feat: webpack add css minimizer

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,24 +1,59 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import path from 'path';
-import { Configuration as WebpackConfiguration } from 'webpack';
+import { Configuration as WebpackConfiguration, WebpackPluginInstance } from 'webpack';
 import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+// eslint-disable-next-line import/no-unresolved
+import TerserPlugin from 'terser-webpack-plugin';
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 
 interface Configuration extends WebpackConfiguration {
   devServer: WebpackDevServerConfiguration;
 }
 
-export const DEV_SERVER_PORT = 3000;
+const DEV_SERVER_PORT = 3000;
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const getMinimizerConfig = (): WebpackPluginInstance[] | undefined => {
+  if (!isDev) {
+    return [
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+    ];
+  }
+
+  return undefined;
+};
 
 const config: Configuration = {
   entry: './src/index.tsx',
   output: {
-    filename: '[name].[contenthash].js',
+    filename: isDev ? '[name].js' : '[name].[contenthash].js',
     path: path.resolve(__dirname, 'public'),
     // TODO: uncomment this
     // clean: true,
   },
+  optimization: {
+    minimizer: getMinimizerConfig(),
+    minimize: true,
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      eslint: { files: './src/**/*.{ts,tsx,js}' },
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      minify: { collapseWhitespace: !isDev },
+      favicon: './src/assets/images/favicon.ico',
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDev ? '[name].css' : 'css/[name].[contenthash].css',
+    }),
+  ],
   module: {
     rules: [
       {
@@ -28,38 +63,33 @@ const config: Configuration = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|ico)$/i,
+        test: /\.(png|svg|jpg|jpeg)$/i,
         type: 'asset/resource',
         generator: { filename: 'images/[name].[contenthash][ext][query]' },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
-        generator: { filename: 'fonts/[name].[contenthash].[ext]' },
+        generator: { filename: 'fonts/[name].[contenthash][ext]' },
       },
     ],
   },
-  plugins: [
-    new ForkTsCheckerWebpackPlugin({
-      async: false,
-      eslint: { files: './src/**/*.{ts,tsx,js}' },
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/index.html'),
-      favicon: './src/assets/images/favicon.ico',
-    }),
-  ],
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
     extensions: ['.ts', '.tsx', '.js'],
   },
+  devtool: isDev ? 'source-map' : false,
   devServer: {
     static: path.resolve(__dirname, 'public'),
     port: DEV_SERVER_PORT,
     historyApiFallback: true,
+    hot: isDev,
   },
 };
 
