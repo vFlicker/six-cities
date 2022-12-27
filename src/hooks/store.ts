06f4d4bb0ac1a44b0e6002f50349c015e3839805
Vladislav <flickervladislav@gmail.com>
2022-12-27 17:34:17 +0200

    refactor: update store types

--- src/hooks/store.ts
+++ src/hooks/store.ts
@@ -2,6 +2,5 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 import { AppDispatch, State } from '~/types';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
