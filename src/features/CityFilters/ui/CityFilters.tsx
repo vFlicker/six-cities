ce32c7e65b0eff80769213f6cbb45509c1761ded
Vladislav <flickervladislav@gmail.com>
2023-02-28 17:00:51 +0200

    feat: sync filtered hotels with query params

--- src/features/CityFilters/ui/CityFilters.tsx
+++ src/features/CityFilters/ui/CityFilters.tsx
@@ -1,7 +1,9 @@
-import { MouseEvent } from 'react';
+import { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 import { filtersModel } from '~/entities/filters';
 import { useAppDispatch, useAppSelector } from '~/shared/hooks';
+import { CityName } from '~/shared/types/hotel';
 import { ButtonLink } from '~/shared/ui/ButtonLink';
 
 import { cityFilters } from '../config';
@@ -10,20 +12,22 @@ import classes from './CityFilters.module.css';
 export function CityFilters(): JSX.Element {
   const dispatch = useAppDispatch();
 
+  const [searchParams] = useSearchParams();
   const cityFilter = useAppSelector(filtersModel.selectCityFilter);
 
-  const filtersList = Array.from(cityFilters).map((city) => {
-    const handleClick = (evt: MouseEvent<HTMLAnchorElement>) => {
-      evt.preventDefault();
-      dispatch(filtersModel.changeCityFilter(city));
-    };
+  useEffect(() => {
+    const filter = searchParams.get('city') as CityName | null;
+    if (!filter) return;
+    dispatch(filtersModel.changeCityFilter(filter));
+  }, [dispatch, searchParams]);
 
+  const filtersList = Array.from(cityFilters).map((city) => {
     const to = `?city=${city}`;
     const isActive = cityFilter === city;
 
     return (
       <li key={city}>
-        <ButtonLink to={to} isActive={isActive} onClick={handleClick}>
+        <ButtonLink to={to} isActive={isActive}>
           {city}
         </ButtonLink>
       </li>
