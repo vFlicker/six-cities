b8f185805bb4e40693e99d79af9ac16c3d780fc2
Vladyslav Sliusar <flickervladislav@gmail.com>
2024-08-06 22:25:37 +0300

    chore: add vite-plugin-checker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,17 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    checker({
+      typescript: {
+        tsconfigPath: './tsconfig.app.json',
+      },
+    }),
+  ],
 });
