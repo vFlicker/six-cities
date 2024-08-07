da64d45ce5cb85a676d9ed10ca5426b797706f79
Vladyslav Sliusar <flickervladislav@gmail.com>
2024-08-07 20:56:13 +0300

    feat: add Icon component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,12 @@ import { Global } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import { Button } from './elements/Button';
+import { Icon, IconSize } from './elements/Icon';
 import { Typography, TypographyVariant } from './elements/Typography';
 import { withAttrs } from './helpers/withAttrs';
 import { Color, globalColors } from './tokens/colors';
 import { globalFonts } from './tokens/fonts';
+import { IconName, SpriteWithIcons } from './tokens/icons';
 import { globalNormalize } from './tokens/normalize';
 import { globalRadiuses } from './tokens/radiuses';
 import { globalResets } from './tokens/resets';
@@ -26,6 +28,20 @@ const StyledText = withAttrs(Typography, {
   color: Color.ORANGE_10,
 });
 
+const StyledButton = styled(Button)`
+  margin-bottom: 10px;
+`;
+
+const StyledStarIcon = withAttrs(Icon, {
+  name: IconName.STAR,
+  size: IconSize.LARGE,
+});
+
+const StyledBookmarkIcon = withAttrs(Icon, {
+  name: IconName.BOOKMARK,
+  size: IconSize.LARGE,
+});
+
 function App(): JSX.Element {
   return (
     <>
@@ -39,7 +55,18 @@ function App(): JSX.Element {
       <StyledText>
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
       </StyledText>
-      <Button>Click me</Button>
+
+      <StyledButton>Click me</StyledButton>
+
+      <div>
+        <Icon name={IconName.ARROW_SELECT} />
+        <Icon name={IconName.BOOKMARK} />
+        <Icon name={IconName.STAR} size={IconSize.MEDIUM} />
+        <StyledStarIcon />
+        <StyledBookmarkIcon />
+      </div>
+
+      <SpriteWithIcons />
     </>
   );
 }
---
+++ client/src/elements/Icon.tsx
@@ -0,0 +1,72 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { PropsWithChildren } from 'react';
+
+import { Color } from '~/tokens/colors';
+import { IconName } from '~/tokens/icons';
+
+type IconProps = PropsWithChildren<{
+  name: IconName;
+  size?: IconSize;
+}>;
+
+const enum IconSize {
+  SMALL = 'small',
+  MEDIUM = 'medium',
+  LARGE = 'large',
+}
+
+const IconNameToIconSize = {
+  [IconName.ARROW_SELECT]: IconSize.SMALL,
+  [IconName.BOOKMARK]: IconSize.MEDIUM,
+  [IconName.STAR]: IconSize.LARGE,
+};
+
+const IconNameToCSS = {
+  [IconName.ARROW_SELECT]: css`
+    fill: ${Color.GRAY_90};
+  `,
+  [IconName.BOOKMARK]: css`
+    fill: transparent;
+    stroke: ${Color.GRAY_50};
+  `,
+  [IconName.STAR]: css`
+    fill: ${Color.GRAY_40};
+  `,
+};
+
+const IconSizeToCSS = {
+  [IconSize.SMALL]: css`
+    width: 8px;
+    height: 8px;
+  `,
+  [IconSize.MEDIUM]: css`
+    width: 20px;
+    height: 20px;
+  `,
+  [IconSize.LARGE]: css`
+    width: 34px;
+    height: 34px;
+  `,
+};
+
+const StyledIcon = styled.svg<IconProps>(({ name, size }) => {
+  const defaultIconSize = size || IconNameToIconSize[name];
+
+  return css`
+    ${IconSizeToCSS[defaultIconSize]};
+    ${IconNameToCSS[name]};
+  `;
+});
+
+function Icon(props: IconProps) {
+  const xlinkHref = `#${props.name}`;
+
+  return (
+    <StyledIcon xmlns="http://www.w3.org/2000/svg" {...props}>
+      <use xlinkHref={xlinkHref} />
+    </StyledIcon>
+  );
+}
+
+export { Icon, IconSize };
